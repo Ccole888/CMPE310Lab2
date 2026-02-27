aa6b487f7311f3d73c8f7a41dc7f561639db566d
Colin Cole <110618749+Ccole888@users.noreply.github.com>
2026-02-27 16:24:19 +0000

    messed up

--- HamDist.s
+++ HamDist.s
@@ -3,51 +3,10 @@
 .lcomm ram, 2048
 
 
-.section .data 
-    msgOne: .ascii "this is a test"
-    lenOne = . - msgOne
-    msgTwo: .ascii "of the emergency broadcast"
-    lenTwo = . - msgTwo
-
 .section .text
     .global hamDist
 
 hamDist:
-    xor %rsi, %rsi
-    xor %rdi, %rdi
-    movq $0, %r12
-    lea msgOne, %rsi
-    lea msgTwo, %rdi
-    movw $lenOne, %ax
-    cmpw $lenTwo, %ax
-    jl less_than
-    lea msgOne, %rsi
-    lea msgTwo, %rdi
-
     
-greater_than:
-    mov $lenTwo, %cl
-    jmp L1
-
-less_than:
-    mov $lenOne, %cl
-    jmp L1    
-
-L1:
-    movb (%rsi), %al
-    movb (%rdi), %bl
-    xorb %al, %bl
-    mov $8, %rdx
-    bitCount:
-        shrb %bl
-        adcq $0, %r12
-        decq %rdx
-        jnz bitCount 
-    incq %rsi
-    incq %rdi
-    dec %cl
-    jnz L1
-
-    movq %r12, ram+0x0
     ret
 .section .note.GNU-stack,"",@progbits
--- HamHelper.c
+++ HamHelper.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 extern unsigned char ram[];
-extern void hamDist();
+extern int hamDist(int size1, int size2);
 
 int main() {
 
+    char m1[256];
+    char m2[256];
+    printf("enter the first message: ");
+    fgets(m1, sizeof(m1), stdin);
+    printf("enter the second message: ");
+    fgets(m2, sizeof(m2), stdin);
+    for(int i = 0; i < sizeof(m1); i++) {
+        ram[i] = m1[i];
+    }
+    for(int i = 0; i < sizeof(m2); i++) {
+        ram[256 + i] = m2[i];
+    }
+    int result = hamDist(sizeof(m1), sizeof(m2));
 
-    hamDist();
-
-    unsigned long long result = *(unsigned long long*)&ram[0x0];
-    printf("Hamming distance: %llu\n", result);
+    printf("Hamming distance: %d\n", result);
     return 0;
 }
